20dc6cdcf7c541e14c7d35bd071230167368c4d3
Nick Miller <nmillerns@hotmail.com>
2014-10-17 23:38:52 -0400

    Implemented win conditions fixed gameover flash by gameover refactor completed implementation TODO: comment and tighten up

--- examples/2048.py
+++ examples/2048.py
@@ -59,10 +59,8 @@ class TwentyFourtyEightGameUI():
 	"""
 	def __init__(self):
 		self.screen = tg.character_display.CharacterDisplay(80, 23) # the game UI creates a 80 x 24 character screen to draw the game upon
-		self.grid = tg.character_map.parse( "    " + "\n"\
-		                                    "    " + "\n"\
-		                                    "    " + "\n"\
-		                                    "    ")
+		self.grid = tg.character_map.CharacterMap(4,4)
+		self.grid.fill(' ')
 		self.prev = self.grid.clone()
 		self.done = False
 		self.delay = 1/8.0
@@ -118,22 +116,12 @@ class TwentyFourtyEightGameUI():
 		"""
 		The UI shows game over screen for 2.5 seconds
 		"""
-		self.screen.fill(' ')
-		self.screen.write_text(30, 10, "Game Over!") # write Game Over!
+		self.render_grid()
+		self.screen.write_text(6, 13, "Game Over!") # write Game Over!
 		self.screen.show() # make sure to .show() so its visible on the console
 		tg.time.sleep(1.5) # sleep with no key presses
-		tg.keyboard.getch(1.0) # wait for a key press for the last second. Also clears the keypresses for the next screen
+		tg.keyboard.getch(10.0) # wait for a key press for the last second. Also clears the keypresses for the next screen
 		
-	def show_win(self):
-		"""
-		The UI shows game winner screen for 2.5 seconds
-		"""
-		self.screen.fill(' ')
-		self.screen.write_text(30, 10, "You Win!\nCongratulations!") # Write you win
-		self.screen.show() # make sure to .show() so its visible on the console
-		tg.time.sleep(1.5) # sleep with no key presses
-		tg.keyboard.getch(1.0) # wait for a key press for the last second. Also clears the keypresses for the next screen
-
 	def score_tile(self, tile):
 		self.score += 2**(ord(tile)+1+1) # score the power of two just achived... +1 for 0 offeset
 
@@ -229,82 +217,75 @@ class TwentyFourtyEightGameUI():
 		self.slide_up()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.combine_up()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.slide_up()
 		
 	def down(self):
 		self.slide_down()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.combine_down()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.slide_down()
 
 	def left(self):
 		self.slide_left()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.combine_left()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.slide_left()
+
 	def right(self):
 		self.slide_right()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.combine_right()
 		self.render_grid()
 		self.screen.show()
-		tg.Metronome(self.delay).wait_for_tick()
+		tg.time.sleep(self.delay)
 		self.slide_right()
 
 	def render_grid(self):
 		self.screen.fill(' ')
 
-		self.screen.write_text(1, 0, "Score: %d"%self.score)
-		self.screen.write_text(1, 1, "Best:  %d"%max(self.highscore.highscoresDB.best(), self.score))
-		if self.won: self.screen.write_text(4, 12, "2048! You Win!")
 		for x in xrange(0, 4):
 			for y in xrange(0, 4):
 				b = self.grid[x, y]
 				if b == ' ': continue
+				if ord(b) >= 9: self.win = True
 				Bxy = TILES[ord(b)]
 				self.screen.draw(5*x, 2*y+2 , Bxy)
+		self.screen.write_text(1, 0, "Score: %d"%self.score)
+		self.screen.write_text(1, 1, "Best:  %d"%max(self.highscore.highscoresDB.best(), self.score))
+		if self.won: self.screen.write_text(4, 12, "2048! You Win!")
 
 	def play(self):
 		"""
 		The UI plays a round of 2048 game. 
 		"""
 		self.done = False
-		#self.grid.fill('\x02')
-
-		#for y in xrange(0,4):
-		#	for x in xrange(y%2, 4,2):
-		#		self.grid[x,y] = '\x03'
-		#self.grid[0,0] = ' '
-		#self.grid[0,2] = '\x04'
-		#self.grid[0,3] = '\x05'
-		#self.grid[0,1] = '\x06'
-		#self.grid[0,1] = '\x06'
 
 		while not self.done: # play forever until something happens
+			for c in self.coords:
+				if self.grid[c] != ' ' and ord(self.grid[c]) >= 10: self.won = True
 			self.render_grid()
-			self.screen.show() # show the updated screen to the user once per frame -- riiiiiiiiiight NOW!
+			self.screen.show()
 			if not self.move_exists():
-				tg.keyboard.getch(1)
-				tg.keyboard.getch(1)
 				self.show_gameover()
 				self.done = True
+				break
 
 			self.prev.draw(0, 0, self.grid)
 
